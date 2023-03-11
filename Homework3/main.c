8f5df31904c64ab02831cf710d4285ad903678fc
tribui1912 <56878689+tribui1912@users.noreply.github.com>
2023-03-10 17:18:45 -0800

    Add files via upload

---
+++ Homework3/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int arrayAddress(int row, int col, int ncolumns, int array[3][5])
+{
+	printf("Address of that element is: %p", array[row][col]);
+	return 0;
+}
+
+
+int main()
+{
+	int array[3][5] = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15}};
+	int row,col;
+
+	printf("Enter row number: ");
+	scanf("%d",row);
+
+	printf("Enter column number: ");
+	scanf("%d",col);
+
+	arrayAddress(row,col,5,array);
+
+	return 0;
+
+}
+
