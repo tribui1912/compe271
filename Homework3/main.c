a866e134ae37913f1b53a70ad32b3630c8ca4b00
tribui1912 <56878689+tribui1912@users.noreply.github.com>
2023-03-10 17:59:07 -0800

    Add files via upload

--- Homework3/main.c
+++ Homework3/main.c
@@ -1,23 +1,33 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int arrayAddress(int row, int col, int ncolumns, int array[3][5])
+/// using define to make changes easier in case want to make the array bigger without having to change too much data
+#define r 3
+#define c 5
+
+int arrayAddress(int row, int col, int ncolumns, int array[r][c])
 {
-	printf("Address of that element is: %p", array[row][col]);
+	printf("Address of that element is: %p\n", array[row][col]);
 	return 0;
 }
 
 
 int main()
 {
-	int array[3][5] = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15}};
-	int row,col;
+	int array[r][c] = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15}};
+	int row = r + 1,col = c + 1;
+	
+	while(row > r && row < 0)
+	{
+		printf("Enter row number: ");
+		scanf("%d",&row);
+	}
 
-	printf("Enter row number: ");
-	scanf("%d",&row);
-
-	printf("Enter column number: ");
-	scanf("%d",&col);
+	while(col > c && col < 0)
+	{
+		printf("Enter column number: ");
+		scanf("%d",&col);
+	}
 
 	arrayAddress(row,col,5,array);
 
