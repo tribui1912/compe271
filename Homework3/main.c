9c38035f2be56ba62dbfca822ae2ff13f8b69aa3
tribui1912 <56878689+tribui1912@users.noreply.github.com>
2023-03-10 18:24:38 -0800

    Add files via upload

--- Homework3/main.c
+++ Homework3/main.c
@@ -17,17 +17,20 @@ int main()
 	int array[r][c] = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15}};
 	int row = r + 1,col = c + 1;
 	
-	while(row > r && row < 0)
+	while(row > r || row < 0)
 	{
 		printf("Enter row number: ");
 		scanf("%d",&row);
 	}
 
-	while(col > c && col < 0)
+	while(col > c || col < 0)
 	{
 		printf("Enter column number: ");
 		scanf("%d",&col);
 	}
+
+	row--;
+	col--;
 
 	arrayAddress(row,col,5,array);
 
